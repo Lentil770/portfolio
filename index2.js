6d89c77f0f2093cca026c30d96ce5cf978cc9db0
Lentil770 <42477416+Lentil770@users.noreply.github.com>
2019-12-28 20:38:00 -0500

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,8 +1,3 @@
-/**function projectsLink() {
-    $('.projects-link').on('click', function() {
-    $('main').scrollTo($('.projects'), 500);
-})};
-**/
 $(".projects-link").click(function() {
     $([document.documentElement, document.body]).animate({
         scrollTop: $(".projects").offset().top
